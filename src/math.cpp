49fa630ae692fbac1cf7990413634d2db5332cb3
pinjuf <paul.denise9110@gmail.com>
2022-08-10 20:56:53 +0200

    Introduce reverse polish notation

--- src/math.cpp
+++ src/math.cpp
@@ -1,4 +1,8 @@
 #include <cmath>
+#include <vector>
+#include <stack>
+#include <string.h>
+#include <sstream>
 
 #include "math.h"
 
@@ -30,4 +34,80 @@ void ifft(double * in, double * out, int n, int sign) {
     for (int i = 0; i < n; i++) {
         out[i] /= n;
     }
+}
+
+int rpc(std::string in, double * out) {
+    std::stack<double> val_stack;
+    double a, b;
+    std::vector<std::string> input;
+    std::stringstream ss(in);
+    std::string tok;
+    while(getline(ss, tok, ' ')) {
+        if (!tok.empty())
+            input.push_back(tok);
+    }
+
+    for (std::string comm : input) {
+        try {
+            val_stack.push(stod(comm, NULL));
+        } catch (std::invalid_argument) {
+            if (!strcmp(comm.c_str(), "+")) {
+                if (val_stack.size() < 2) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                b = val_stack.top();val_stack.pop();
+                val_stack.push(a+b);
+            }   
+            else if (!strcmp(comm.c_str(), "*")) {
+                if (val_stack.size() < 2) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                b = val_stack.top();val_stack.pop();
+                val_stack.push(a*b);
+            }   
+            else if (!strcmp(comm.c_str(), "-")) {
+                if (val_stack.size() < 2) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                b = val_stack.top();val_stack.pop();
+                val_stack.push(b-a);
+            }   
+            else if (!strcmp(comm.c_str(), "/")) {
+                if (val_stack.size() < 2) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                b = val_stack.top();val_stack.pop();
+                val_stack.push(b/a);
+            }
+
+            else if (!strcmp(comm.c_str(), "^")) {
+                if (val_stack.size() < 1) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                b = val_stack.top();val_stack.pop();
+                val_stack.push(pow(b, a));
+            }   
+            else if (!strcmp(comm.c_str(), "v")) {
+                if (val_stack.size() < 1) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                val_stack.push(sqrt(a));
+            }   
+            else if (!strcmp(comm.c_str(), "ln")) {
+                if (val_stack.size() < 1) {return -1;}
+                a = val_stack.top();val_stack.pop();
+                val_stack.push(log(a));
+            }   
+
+            else if (!strcmp(comm.c_str(), "sum")) {
+                a = 0;
+                while (!val_stack.empty()) {
+                    a += val_stack.top();val_stack.pop();
+                }
+                val_stack.push(a);
+            }
+        }
+    }
+
+    if (val_stack.empty()) {
+        return -1;
+    }
+
+    *out = val_stack.top();
+
+    return 0;
 }
--- src/math.h
+++ src/math.h
@@ -1,7 +1,11 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <string>
+
 void fft(double * in, double * out, int n, int sign);
 void ifft(double * in, double * out, int n, int sign);
+
+int rpc(std::string in, double * out);
 
 #endif
